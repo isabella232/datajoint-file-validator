ea91910e1b891786afe0e03c2007bf1e7022e3d9
Ethan Ho <ethan.n.ho@gmail.com>
2023-12-05 17:02:49 -0600

    Add typer CLI example

---
+++ datajoint_file_validator/cli.py
@@ -0,0 +1,38 @@
+import typer
+from . import app
+
+
+@app.callback()
+def callback():
+    """
+    Awesome Portal Gun
+    """
+
+
+@app.command()
+def shoot():
+    """
+    Shoot the portal gun
+    """
+    typer.echo("Shooting portal gun")
+
+
+@app.command()
+def load():
+    """
+    Load the portal gun
+    """
+    typer.echo("Loading portal gun")
+
+
+@app.command()
+def main(name: str, lastname: str = "", formal: bool = False):
+    """
+    Say hi to NAME, optionally with a --lastname.
+
+    If --formal is used, say hi very formally.
+    """
+    if formal:
+        print(f"Good day Ms. {name} {lastname}.")
+    else:
+        print(f"Hello {name} {lastname}")
