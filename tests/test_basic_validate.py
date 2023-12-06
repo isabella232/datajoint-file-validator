cbfaf4841b6a1de141577625262b0a5ac4e3f97c
Ethan Ho <ethan.n.ho@gmail.com>
2023-12-05 22:29:21 -0600

    Progress

--- tests/test_basic_validate.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import glob
-import datajoint_file_validator as djfval
-
-
-@pytest.mark.parametrize('manifest_path', glob.glob('datajoint_file_validator/manifests/*.yaml'))
-def test_built_in_manifests(manifest_path):
-	djfval.validate(snapshot=snapshot, manifest=manifest_path, verbose=True, raise_err=False)
---
+++ tests/test_fileset_validate.py
@@ -0,0 +1,11 @@
+import pytest
+import glob
+import datajoint_file_validator as djfval
+
+
+@pytest.mark.parametrize('manifest_path,fileset_path', (
+    ('datajoint_file_validator/manifests/demo_dlc_v0.1.demo_dlc_v0.1.yaml', 'tests/data/filesets/fileset0'),
+))
+def test_validate_built_in_filesets(manifest_path, fileset_path):
+    djfval.snapshot(fileset_path)
+    djfval.validate(snapshot=snapshot, manifest=manifest_path, verbose=True, raise_err=False)
