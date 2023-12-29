1366480ddbfce0f4b199fcb6bf18b2cce2303a54
Ethan Ho <ethan.n.ho@gmail.com>
2023-12-29 12:46:19 -0700

    Add BaseSettings implementation
    Inspired by Pydantic's BaseSettings
    https://github.com/pydantic/pydantic-settings

--- datajoint_file_validator/config.py
+++ datajoint_file_validator/config.py
@@ -1,14 +1,19 @@
-from dataclasses import dataclass
+from pathlib import Path
+from .base_settings import BaseSettings
 
 
-@dataclass
-class Config:
-    """Config class for the application"""
+class Config(BaseSettings):
+    """Config class for the application. Mimics behavior of Pydantic's BaseSettings."""
+
+    ENV_PATH = ".env"
 
     allow_eval: bool = True
     debug: bool = True
     enable_path_handle: bool = True
     default_query: str = "**"
+    manifest_schema: Path = Path(
+        "datajoint_file_validator/manifest_schemas/v0.1.0.yaml"
+    )
 
 
 config = Config()
